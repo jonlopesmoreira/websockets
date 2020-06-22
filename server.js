1e2cd69b0e6c6e6687c8e24e4c0d1ede21b46a59
jonlopesmoreira <jonprojetos@hotmail.com>
2020-06-22 10:55:11 -0300

    Socket

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app); // protocolo http
+const io = require('socket.io')(server); // protocolo websocket
+
+app.use(express.static(path.join(__dirname, 'public'))); // pasta onde ficam os arquivos publicos, a partir do diretorio atual acessa a pasta public
+app.set('views', path.join(__dirname,'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+    socket.on('sendMessage', data => {
+        console.log(data);
+    });
+});
+server.listen(3000);
