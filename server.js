f0bbf3a4dbd255602db894dd9dd4a550c1ba978e
jonlopesmoreira <jonprojetos@hotmail.com>
2020-06-22 12:15:50 -0300

    Added some comments

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(server); // protocolo websocket
 
 app.use(express.static(path.join(__dirname, 'public'))); // pasta onde ficam os arquivos publicos, a partir do diretorio atual acessa a pasta public
 app.set('views', path.join(__dirname,'public'));
-app.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile); // utilizar o html para views
 app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
@@ -16,12 +16,13 @@ app.use('/', (req, res) => {
 
 let messages = [];
 
-io.on('connection', socket => {
+io.on('connection', socket => {  // estabelece a conexao
     console.log(`Socket conectado: ${socket.id}`);
     socket.emit('previousMessages', messages);
     socket.on('sendMessage', data => {
         messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
+        socket.broadcast.emit('receivedMessage', data); // envia para tds sockets o data que contem author e texto
     });
 });
 server.listen(3000);
+console.log("Escutando em http://localhost:3000");
